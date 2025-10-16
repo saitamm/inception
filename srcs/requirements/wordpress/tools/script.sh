e847d762899eaf7b50742fcf7043abc4ea3ad4df
soumaya <soumaya@soumaya.soumaya>
2025-10-16 11:46:52 -0400

    problem

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,37 +1,37 @@
 #!bin/bash
 
-# echo "DB_NAME=$MYSQL_DATABASE"
-# echo "DB_USER=$MYSQL_USER"
-# echo "DB_PASS=$MYSQL_PASSWORD"
-# echo "DB_HOST=$DB_HOST:$DB_HOST_PORT"
+echo "DB_NAME=$MYSQL_DATABASE"
+echo "DB_USER=$MYSQL_USER"
+echo "DB_PASS=$MYSQL_PASSWORD"
+echo "DB_HOST=$DB_HOST:$DB_HOST_PORT"
 
-# until mysqladmin ping -h"$DB_HOST" -P"$DB_HOST_PORT" --silent; do
-# #   echo "Waiting for MariaDB..."
-#   sleep 2
-# done
+until mysqladmin ping -h"$DB_HOST" -P"$DB_HOST_PORT" --silent; do
+  echo "Waiting for MariaDB..."
+  sleep 2
+done
 
-# if [ ! -f /var/www/html/wp-config.php ]; then
-# 	wp core download --allow-root
-# 	wp config create \
-# 		--dbname="$MYSQL_DATABASE" \
-# 		--dbuser="$MYSQL_USER" \
-# 		--dbpass="$MYSQL_PASSWORD" \
-# 		--dbhost="$DB_HOST:$DB_HOST_PORT" \
-# 		--allow-root
-# fi
-# if ! wp core is-installed --allow-root; then
-#   wp core install \
-#     --url="https://sait-amm.42.fr" \
-#     --title="Inception" \
-#     --admin_user="$MYSQL_USER" \
-#     --admin_password="$MYSQL_PASSWORD" \
-#     --admin_email="$USER_EMAIL" \
-#     --allow-root
-# fi
+if [ ! -f /var/www/html/wp-config.php ]; then
+	wp core download --allow-root
+	wp config create \
+		--dbname="$MYSQL_DATABASE" \
+		--dbuser="$MYSQL_USER" \
+		--dbpass="$MYSQL_PASSWORD" \
+		--dbhost="$DB_HOST:$DB_HOST_PORT" \
+		--allow-root
+fi
+if ! wp core is-installed --allow-root; then
+  wp core install \
+    --url="https://sait-amm.42.fr" \
+    --title="Inception" \
+    --admin_user="$MYSQL_USER" \
+    --admin_password="$MYSQL_PASSWORD" \
+    --admin_email="$USER_EMAIL" \
+    --allow-root
+fi
 
-# echo "USER=$USER"
-# echo "USER_EMAIL=$USER_EMAIL"
-# echo "USER_PASSWORD=$USER_PASSWORD"
+echo "USER=$USER"
+echo "USER_EMAIL=$USER_EMAIL"
+echo "USER_PASSWORD=$USER_PASSWORD"
 
 # echo "soumaaaaaaaaaya wordpress started"
 
